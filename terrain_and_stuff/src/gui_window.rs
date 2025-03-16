801f6c103c279d1755e6fc52d2119edca28bcdb9
Andreas Reich <r_andreas2@web.de>
2025-03-16 15:30:17 +0100

    ui styling

--- terrain_and_stuff/src/gui_window.rs
+++ terrain_and_stuff/src/gui_window.rs
@@ -17,9 +17,7 @@ pub fn run_gui(
         egui::CollapsingHeader::new("Atmosphere")
             .default_open(true)
             .show(ui, |ui| {
-                egui::Grid::new("atmosphere_grid").show(ui, |ui| {
-                    atmosphere_settings(ui, atmosphere);
-                });
+                atmosphere_settings(ui, atmosphere);
             });
 
         egui::CollapsingHeader::new("Camera")
@@ -54,14 +52,6 @@ pub fn run_gui(
 }
 
 fn atmosphere_settings(ui: &mut egui::Ui, atmosphere: &mut Atmosphere) {
-    ui.label("Sun azimuth");
-    ui.drag_angle(&mut atmosphere.sun_azimuth);
-    ui.end_row();
-
-    ui.label("Sun altitude");
-    ui.drag_angle(&mut atmosphere.sun_altitude);
-    ui.end_row();
-
     let AtmosphereParams {
         draw_mode,
         ground_radius_km,
@@ -79,132 +69,145 @@ fn atmosphere_settings(ui: &mut egui::Ui, atmosphere: &mut Atmosphere) {
 
     let default_params = AtmosphereParams::default();
 
-    let mut draw_mode_enum = draw_mode.get();
-    ui.label("Debug draw mode");
-    egui::ComboBox::from_id_salt("atmosphere_debug_draw_mode")
-        .selected_text(format!("{:?}", draw_mode_enum))
-        .show_ui(ui, |ui| {
-            for variant_val in
-                AtmosphereDebugDrawMode::MIN_VALUE..=AtmosphereDebugDrawMode::MAX_VALUE
-            {
-                let variant = AtmosphereDebugDrawMode::from_integer(variant_val).unwrap();
-                ui.selectable_value(&mut draw_mode_enum, variant, variant.to_string());
-            }
+    egui::Grid::new("atmosphere_grid").show(ui, |ui| {
+        ui.label("Sun azimuth");
+        ui.drag_angle(&mut atmosphere.sun_azimuth);
+        ui.end_row();
+
+        ui.label("Sun altitude");
+        ui.drag_angle(&mut atmosphere.sun_altitude);
+        ui.end_row();
+
+        let mut draw_mode_enum = draw_mode.get();
+        ui.label("Debug draw mode");
+        egui::ComboBox::from_id_salt("atmosphere_debug_draw_mode")
+            .selected_text(format!("{:?}", draw_mode_enum))
+            .show_ui(ui, |ui| {
+                for variant_val in
+                    AtmosphereDebugDrawMode::MIN_VALUE..=AtmosphereDebugDrawMode::MAX_VALUE
+                {
+                    let variant = AtmosphereDebugDrawMode::from_integer(variant_val).unwrap();
+                    ui.selectable_value(&mut draw_mode_enum, variant, variant.to_string());
+                }
+            });
+        draw_mode.set(draw_mode_enum);
+        ui.end_row();
+    });
+
+    egui::CollapsingHeader::new("Advanced")
+        .default_open(false)
+        .show(ui, |ui| {
+            egui::Grid::new("advanced_atmosphere").show(ui, |ui| {
+                ui.label("Ground radius (km)");
+                with_default(
+                    ui,
+                    ground_radius_km,
+                    default_params.ground_radius_km,
+                    |ui, v| {
+                        ui.add(egui::DragValue::new(v));
+                    },
+                );
+                ui.end_row();
+
+                ui.label("Atmosphere radius (km)");
+                with_default(
+                    ui,
+                    atmosphere_radius_km,
+                    default_params.atmosphere_radius_km,
+                    |ui, v| {
+                        ui.add(egui::DragValue::new(v));
+                    },
+                );
+                ui.end_row();
+
+                ui.separator();
+                ui.end_row();
+
+                ui.label("Sun illuminance");
+                with_default(
+                    ui,
+                    sun_illuminance,
+                    default_params.sun_illuminance,
+                    drag_value_vec3_precise_positive,
+                );
+                ui.end_row();
+
+                ui.label("Sun disk diameter");
+                with_default(
+                    ui,
+                    sun_disk_diameteter_rad,
+                    default_params.sun_disk_diameteter_rad,
+                    egui::Ui::drag_angle,
+                );
+                ui.end_row();
+
+                ui.label("Sun disk illuminance factor");
+                with_default(
+                    ui,
+                    sun_disk_illuminance_factor,
+                    default_params.sun_disk_illuminance_factor,
+                    drag_value_f32_precise_positive,
+                );
+                ui.end_row();
+
+                ui.separator();
+                ui.end_row();
+
+                ui.label("Rayleigh scale height");
+                with_default(
+                    ui,
+                    rayleigh_scale_height,
+                    default_params.rayleigh_scale_height,
+                    drag_value_f32_precise_positive,
+                );
+                ui.end_row();
+
+                ui.label("Rayleigh scattering density/km");
+                with_default(
+                    ui,
+                    rayleigh_scattering_per_km_density,
+                    default_params.rayleigh_scattering_per_km_density,
+                    drag_value_vec3_precise_positive,
+                );
+                ui.end_row();
+
+                ui.label("Mie scale height");
+                with_default(
+                    ui,
+                    mie_scale_height,
+                    default_params.mie_scale_height,
+                    drag_value_f32_precise_positive,
+                );
+                ui.end_row();
+
+                ui.label("Mie scattering density/km");
+                with_default(
+                    ui,
+                    mie_scattering_per_km_density,
+                    default_params.mie_scattering_per_km_density,
+                    drag_value_f32_precise_positive,
+                );
+                ui.end_row();
+
+                ui.label("Mie absorption density/km");
+                with_default(
+                    ui,
+                    mie_absorption_per_km_density,
+                    default_params.mie_absorption_per_km_density,
+                    drag_value_f32_precise_positive,
+                );
+                ui.end_row();
+
+                ui.label("Ozone absorption density/km");
+                with_default(
+                    ui,
+                    ozone_absorption_per_km_density,
+                    default_params.ozone_absorption_per_km_density,
+                    drag_value_vec3_precise_positive,
+                );
+                ui.end_row();
+            });
         });
-    draw_mode.set(draw_mode_enum);
-    ui.end_row();
-
-    ui.separator();
-    ui.end_row();
-
-    ui.label("Ground radius (km)");
-    with_default(
-        ui,
-        ground_radius_km,
-        default_params.ground_radius_km,
-        |ui, v| {
-            ui.add(egui::DragValue::new(v));
-        },
-    );
-    ui.end_row();
-
-    ui.label("Atmosphere radius (km)");
-    with_default(
-        ui,
-        atmosphere_radius_km,
-        default_params.atmosphere_radius_km,
-        |ui, v| {
-            ui.add(egui::DragValue::new(v));
-        },
-    );
-    ui.end_row();
-
-    ui.separator();
-    ui.end_row();
-
-    ui.label("Sun illuminance");
-    with_default(
-        ui,
-        sun_illuminance,
-        default_params.sun_illuminance,
-        drag_value_vec3_precise_positive,
-    );
-    ui.end_row();
-
-    ui.label("Sun disk diameter");
-    with_default(
-        ui,
-        sun_disk_diameteter_rad,
-        default_params.sun_disk_diameteter_rad,
-        egui::Ui::drag_angle,
-    );
-    ui.end_row();
-
-    ui.label("Sun disk illuminance factor");
-    with_default(
-        ui,
-        sun_disk_illuminance_factor,
-        default_params.sun_disk_illuminance_factor,
-        drag_value_f32_precise_positive,
-    );
-    ui.end_row();
-
-    ui.separator();
-    ui.end_row();
-
-    ui.label("Rayleigh scale height");
-    with_default(
-        ui,
-        rayleigh_scale_height,
-        default_params.rayleigh_scale_height,
-        drag_value_f32_precise_positive,
-    );
-    ui.end_row();
-
-    ui.label("Rayleigh scattering density/km");
-    with_default(
-        ui,
-        rayleigh_scattering_per_km_density,
-        default_params.rayleigh_scattering_per_km_density,
-        drag_value_vec3_precise_positive,
-    );
-    ui.end_row();
-
-    ui.label("Mie scale height");
-    with_default(
-        ui,
-        mie_scale_height,
-        default_params.mie_scale_height,
-        drag_value_f32_precise_positive,
-    );
-    ui.end_row();
-
-    ui.label("Mie scattering density/km");
-    with_default(
-        ui,
-        mie_scattering_per_km_density,
-        default_params.mie_scattering_per_km_density,
-        drag_value_f32_precise_positive,
-    );
-    ui.end_row();
-
-    ui.label("Mie absorption density/km");
-    with_default(
-        ui,
-        mie_absorption_per_km_density,
-        default_params.mie_absorption_per_km_density,
-        drag_value_f32_precise_positive,
-    );
-    ui.end_row();
-
-    ui.label("Ozone absorption density/km");
-    with_default(
-        ui,
-        ozone_absorption_per_km_density,
-        default_params.ozone_absorption_per_km_density,
-        drag_value_vec3_precise_positive,
-    );
-    ui.end_row();
 }
 
 fn drag_value_vec3<T: Into<glam::Vec3> + From<glam::Vec3> + Copy>(ui: &mut egui::Ui, v: &mut T) {
